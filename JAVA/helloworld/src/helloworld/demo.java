8ae25f2df4a54cca93f977fc4f0edbe483b7767a
chazi-mac <zhaoweicryoem@gmail.com>
2016-05-04 20:16:56 +0800

    a

---
+++ JAVA/helloworld/src/helloworld/demo.java
@@ -0,0 +1,19 @@
+package helloworld;
+import java.util.Scanner;
+public class demo {
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		System.out.print(" 请输入考试成绩");
+		
+		int score=input.nextInt();
+		int count=0;
+		System.out.println("加分前成绩:"+score);
+		while(score<60){
+			score++;
+			count++;
+			
+		}
+		System.out.println("加分前成绩:"+score);
+		System.out.println("加了:"+count+"次！");
+	}
+}
